595826d27f7dbb98c5c688943d90809023c7cdd8
Cesario <Cesarioneto7@gmail.com>
2021-05-20 17:50:56 -0300

    .

--- receitas/models.py
+++ receitas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.db.models.base import ModelState
 from datetime import datetime
+from pessoas.models import Pessoa
 
 class receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
