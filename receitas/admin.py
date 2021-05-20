71b2c343ea286e6a1a626ac71a1245bd69933ed1
Cesario <Cesarioneto7@gmail.com>
2021-05-20 12:27:57 -0300

    adicionando uma classe Listadoreceita em admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import receita
 
-admin.site.register(receita)
+class Listandoreceita(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+
+
+admin.site.register(receita, Listandoreceita)
