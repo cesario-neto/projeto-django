48d4489166ef5e3d669e8649e64f3c8a077ce695
Cesario <Cesarioneto7@gmail.com>
2021-05-20 12:52:52 -0300

    Adicionando mais filtros para o admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import receita
 
 class Listandoreceita(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'tempo_preparo')
+    list_display = ('id', 'nome_receita', 'tempo_preparo', 'categoria')
     list_display_links = ('id', 'nome_receita')
-
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = (3)
 
 admin.site.register(receita, Listandoreceita)
