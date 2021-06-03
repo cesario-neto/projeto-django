2140460f11f9cee107fe777f0f80f90530bcefeb
Cesario <Cesarioneto7@gmail.com>
2021-06-03 15:31:48 -0300

    .

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -1,4 +1,4 @@
-
+from usuarios import urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('receitas.urls')),
+    path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
