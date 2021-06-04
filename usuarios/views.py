d6daecd32080b0c0bc7a2f0c7c73740ed6084212
Cesario <Cesarioneto7@gmail.com>
2021-06-04 13:36:49 -0300

    .

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -6,16 +8,62 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-        print(nome, email, senha, senha2)
+        if not nome.strip():
+             print('O campo nome não pode ficar vazio')
+             return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar vazio')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário ja cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        User.save
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request,'usuarios/cadastro.html')
 
 def login (request):
+    if request.method == 'POST':
+        email = request.POST ['email']
+        senha = request.POST ['senha']
+        if email == '' or senha == '':
+            print('Os campos não podem ficar vazios')
+            return redirect('login')
+        print (email, senha)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
+        return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout (request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard (request):
-    pass
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
+    
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        print(nome_receita, ingredientes, modo_preparo, tempo_preparo, rendimento, categoria, foto_receita)
+    else:
+        return render(request, 'usuarios/cria_receita.html')
